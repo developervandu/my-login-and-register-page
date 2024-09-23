7d32f5a77652f48de844b96f83b6e0adf268ffa1
vandu.. <developer.vandu@gmail.com>
2024-09-23 22:33:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function changeBgImage() {
+  document.getElementById("main").style.backgroundImage =
+    "url('./assets/background-cat.jpg')";
+}
+    function myFunction() {
+      alert("Your Form Submited Successfully");
+
+}
+ function femaleImgChange(){
+  document.getElementById("female-img").style.display = "block";
+  document.getElementById("male-img").style.display = "none";
+ }
+ 
+ function maleImgChange(){
+  document.getElementById("male-img").style.display = "block" ;
+  document.getElementById("female-img").style.display = "none";
+ }
